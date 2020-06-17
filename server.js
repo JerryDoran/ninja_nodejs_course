664a01094b3184c3c4575994348775c6519ff379
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-06-17 11:55:45 -0400

    Finished the server module

--- server.js
+++ server.js
@@ -1,8 +1,52 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
-  
+  console.log(req.url, req.method);
+
+  // set header content type
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.write('hello client');
+  // res.end();
+
+  // res.setHeader('Content-Type', 'text/html');
+  // res.write('<h1>hello client</h1>');
+  // res.write('<p>this is a response from the server</p>');
+  // res.end();
+
+  res.setHeader('Content-Type', 'text/html');
+
+  let path = './views/';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+
+  // send html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
+    }
+  });
 });
 
 port = process.env.port || 3000;
